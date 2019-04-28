0fa1a407a3369dada2bcebf5c337ac60a68525d5
emily <emily@emilys-MacBook-Pro.local>
2019-04-28 18:24:02 -0400

    finished joke and cat pic

---
+++ index.js
@@ -0,0 +1,112 @@
+const Slackbot = require('slackbots');
+const axios = require('axios');
+
+const bot = new Slackbot({
+    token: 'xoxb-619971975700-620534324496-KwSmqmdAzmE7H9anPgyIYm5F',
+    name: 'happyBot'
+});
+
+//Start Handler
+bot.on('start', () => {
+    const params = {
+        icon_emoji: ':smiley:'
+    }
+
+    bot.postMessageToChannel('general', 'Hello! Get Ready to Laugh With @happyBot!',
+        params
+    );
+});
+
+// Erro Handler
+bot.on('error', (err) => console.log(err));
+
+//Message Handler
+bot.on('message', (data) => {
+    if (data.type !== 'message') {
+        return;
+    }
+    handleMessage(data.text);
+})
+
+//Respons to Data
+const handleMessage = (message) => {
+    if (message.includes(' tell me a joke')) {
+        chunckJoke();
+    } else if (message.includes(' yomama')) {
+        yoMamaJoke();
+
+    } else if (message.includes(' random')) {
+        randomJoke();
+    } else if (message.includes(' cat')) {
+        catPic();
+    } else if (message.includes(' help')) {
+        runHelp();
+    }
+}
+
+//Tell a Chunk Norris Joke
+const chunckJoke = () => {
+    axios.get('http://api.icndb.com/jokes/random')
+        .then(res => {
+            const joke = res.data.value.joke;
+
+            const params = {
+                icon_emoji: ':laughing:'
+            }
+
+            bot.postMessageToChannel('general', `Chunk Norris: ${joke}`,
+                params
+            );
+        })
+}
+// Tell a yoMama joke
+const yoMamaJoke = () => {
+    axios.get('https://api.yomomma.info/')
+        .then(res => {
+            const joke = res.data.joke;
+
+            const params = {
+                icon_emoji: ':smirk:'
+            }
+
+            bot.postMessageToChannel('general', `Yo Mama: ${joke}`,
+                params
+            );
+        })
+}
+
+const catPic = () => {
+    axios.get('https://aws.random.cat/meow')
+        .then(res => {
+            const catPicture = res.data.file;
+
+            const params = {
+                icon_emoji: ':cat:'
+            }
+            bot.postMessageToChannel('general', catPicture, params)
+        })
+}
+
+
+
+//Tell a random joke
+const randomJoke = () => {
+    const rand = Math.floor(Math.random() * 2) + 1;
+    if (rand === 1) {
+        chunckJoke();
+    } else if (rand === 2) {
+        yoMamaJoke();
+    }
+}
+
+// Show Help Text
+
+const runHelp = () => {
+    const params = {
+        icon_emoji: ':question:'
+    }
+
+    bot.postMessageToChannel('general', `Type @happyBot with either 'tell me a joke', 'yomama', or 'random' to get a joke, or with 'cat' to get a cat picture`,
+        params
+    );
+}
